4a4336b4fb11e80491ec1af792076b6cb0dacc58
gazielu <46880933+gazielu@users.noreply.github.com>
2023-01-11 22:54:59 +0200

    add streamlit dataframe

--- app_12_blankets_data_from_sql_server/blankets_extracts.py
+++ app_12_blankets_data_from_sql_server/blankets_extracts.py
@@ -1,23 +1,38 @@
 import streamlit as st
 import pyodbc
-
+import pandas as pd
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
 
-conn = init_connection()
 
+# @st.experimental_singleton
+# def init_connection():
+#     return pyodbc.connect(
+#         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+#         + st.secrets["server"]
+#         + ";DATABASE="
+#         + st.secrets["database"]
+#         + ";UID="
+#         + st.secrets["username"]
+#         + ";PWD="
+#         + st.secrets["password"]
+#     )
+
+# conn = init_connection()
+server = 'gvs72069.inc.hpicorp.net,2048' 
+database = 'GABI-P' 
+username = 'GABI_STG_RW_1' 
+password = 'fT7_E*4U6k_m' 
+odbc_GABI_P = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+dynamic_sql = '''
+SELECT top 10 from [DMINDWNG_SCHEMA].[V_Quality_Status_Change_Reason_Dimension_Hierarchy]
+'''
+
+pandas_read_pdf = pd.read_sql_query(sql = dynamic_sql,\
+                                        con = odbc_GABI_P)
+
+st.dataframe(pandas_read_pdf)
 # # Perform query.
 # # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 # @st.experimental_memo(ttl=600)
