a69279c2afe1d7904e195fcd7dec05748aab8653
gazielu <46880933+gazielu@users.noreply.github.com>
2023-01-11 23:07:56 +0200

    pyodbc

--- app_12_blankets_data_from_sql_server/blankets_extracts.py
+++ app_12_blankets_data_from_sql_server/blankets_extracts.py
@@ -5,32 +5,32 @@ import pandas as pd
 # Uses st.experimental_singleton to only run once.
 
 
-# @st.experimental_singleton
-# def init_connection():
-#     return pyodbc.connect(
-#         "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-#         + st.secrets["server"]
-#         + ";DATABASE="
-#         + st.secrets["database"]
-#         + ";UID="
-#         + st.secrets["username"]
-#         + ";PWD="
-#         + st.secrets["password"]
-#     )
+@st.experimental_singleton
+def init_connection():
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["server"]
+        + ";DATABASE="
+        + st.secrets["database"]
+        + ";UID="
+        + st.secrets["username"]
+        + ";PWD="
+        + st.secrets["password"]
+    )
 
-# conn = init_connection()
-server = 'gvs72069.inc.hpicorp.net,2048' 
-database = 'GABI-P' 
-username = 'GABI_STG_RW_1' 
-password = 'fT7_E*4U6k_m' 
-odbc_GABI_P = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+conn = init_connection()
+# server = 'gvs72069.inc.hpicorp.net,2048' 
+# database = 'GABI-P' 
+# username = 'GABI_STG_RW_1' 
+# password = 'fT7_E*4U6k_m' 
+# odbc_GABI_P = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
 dynamic_sql = '''
 SELECT top 10 * from [DMINDWNG_SCHEMA].[V_Quality_Status_Change_Reason_Dimension_Hierarchy]
 '''
 
 pandas_read_pdf = pd.read_sql_query(sql = dynamic_sql,\
-                                        con = odbc_GABI_P)
+                                        con = conn)
 
 st.dataframe(pandas_read_pdf)
 # # Perform query.
