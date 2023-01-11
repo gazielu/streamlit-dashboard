26620a2e97a2d6a37a0cccab7dc48487580271cc
gazielu <46880933+gazielu@users.noreply.github.com>
2023-01-11 19:50:08 +0200

    create requirments file

--- app_3_eda_basketball/basketball_app.py
+++ app_3_eda_basketball/basketball_app.py
@@ -47,8 +47,8 @@ st.dataframe(df_selected_team)
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    b64 = pybase64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;pybase64,{b64}" download="playerstats.csv">Download CSV File</a>'
     return href
 
 st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
